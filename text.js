4214dd90918eff71a19c5e04026a7e2b5b16cd36
Olive <141564336+xxOliveXyzxx@users.noreply.github.com>
2023-11-06 14:56:10 -0700

    Update text.js

--- text.js
+++ text.js
@@ -1,7 +1,7 @@
 function dark() {
-    document.getElementById("body").style.background = "black";
-    document.getElementById("body").style.animation = "none";
+    document.getElementById("hulbody").style.background = "black";
+    document.getElementById("hulbody").style.animation = "none";
     document.getElementById("subhead").style.color = "white";
-    document.getElementById("a").style.color = "pink";
+    document.getElementById("an").style.color = "pink";
     document.getElementById("fieldset").style.color = "white";
 }
