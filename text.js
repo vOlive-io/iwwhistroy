ae20e57c11ada8bd7c6517eadf938b4ab0bbc9d2
Olive <141564336+xxOliveXyzxx@users.noreply.github.com>
2023-11-06 15:06:13 -0700

    Update text.js

--- text.js
+++ text.js
@@ -4,4 +4,5 @@ function dark() {
     document.getElementById("subhead").style.color = "white";
     document.getElementById("an").style.color = "pink";
     document.getElementById("fieldset").style.color = "white";
+    document.getElementById("par").style.color = "pink";
 }
