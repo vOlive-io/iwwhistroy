14d2ca8364759de4e8de53ce0eb73c94cee9ef82
Olive <141564336+xxOliveXyzxx@users.noreply.github.com>
2023-11-06 14:53:27 -0700

    Create text.js

---
+++ text.js
@@ -0,0 +1,7 @@
+function dark() {
+    document.getElementById("body").style.background = "black";
+    document.getElementById("body").style.animation = "none";
+    document.getElementById("subhead").style.color = "white";
+    document.getElementById("a").style.color = "pink";
+    document.getElementById("fieldset").style.color = "white";
+}
